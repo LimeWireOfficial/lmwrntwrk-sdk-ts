0568e57bc062b3cf358a413c598c6a4dc0c4906d
haris <haris@limewire.com>
2026-04-14 15:52:57 +0200

    chore: better version handling

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-import pkg from "../package.json" with { type: "json" };
+import { version } from "./version.js";
 export const SignatureHeader = 'x-lmwrntwrk-signature';
 export const RequestIdHeader = 'x-lmwrntwrk-request-id';
 export const ChunkSizeHeader = 'x-lmwrntwrk-chunk-size';
@@ -16,5 +16,5 @@ export const DefaultChunkSize = 10 * 1024 * 1024;
 export const MaxAllowedPresignedRequestDurationSeconds = 30 * 60;
 
 export const DefaultGraphEndpoint = "https://graph.limewire.network/subgraphs/name/lmwrntwrk-v1";
-export const LmwrntwrkSdkVersion = pkg.version;
+export const LmwrntwrkSdkVersion = version;
 export const LmwrntwrkUserAgent = `LmwrNtwrkTsSdk/${LmwrntwrkSdkVersion}`;
